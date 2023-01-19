dae9c91a55610cc84936133b224b6515fc8b669b
edilson madsen <edilson_madsen@hotmail.com>
2023-01-19 15:36:33 -0300

    front-end

---
+++ src/components/common/headerAuth/index.tsx
@@ -0,0 +1,37 @@
+import styles from "./styles.module.scss";
+import { Container, Form, Input } from "reactstrap";
+import Link from "next/link";
+
+const HeaderAuth = function () {
+  return (
+    <>
+      <Container className={styles.nav}>
+        <Link href="/home">
+          <img
+            src="/logoOnebitflix.svg"
+            alt="logoOnebitflix"
+            className={styles.imgLogoNav}
+          />
+        </Link>
+        <div className="d-flex align-items-center">
+          <Form>
+            <Input
+              name="search"
+              type="search"
+              placeholder="Pesquisar"
+              className={styles.input}
+            />
+          </Form>
+          <img
+            src="/homeAuth/iconSearch.svg"
+            alt="lupaHeader"
+            className={styles.searchImg}
+          />
+          <p className={styles.userProfile}>AB</p>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderAuth;
